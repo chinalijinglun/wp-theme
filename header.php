46e429c0b5ccbee075dd3c4bec47f273b14a1d01
lijinglun <281965992@qq.com>
2016-01-13 16:30:08 +0800

    上传我现在写到的wp的主题

---
+++ header.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" >
+  <title><?php if ( is_home() ) {
+        bloginfo('name'); echo " - "; bloginfo('description');
+    } elseif ( is_category() ) {
+        single_cat_title(); echo " - "; bloginfo('name');
+    } elseif (is_single() || is_page() ) {
+        single_post_title();
+    } elseif (is_search() ) {
+        echo "搜索结果"; echo " - "; bloginfo('name');
+    } elseif (is_404() ) {
+        echo '页面未找到!';
+    } else {
+        wp_title('',true);
+    } ?></title>
+    <!-- css -->
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+
+    <!-- 新 Bootstrap 核心 CSS 文件 -->
+	<link href="http://apps.bdimg.com/libs/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
+
+    <!--pingback -->
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+    <!--feed链接    -->
+    <link rel="alternate" type="application/rss+xml" title="RSS 2.0 - 所有文章" href="<?php echo get_bloginfo('rss2_url'); ?>" />
+    <link rel="alternate" type="application/rss+xml" title="RSS 2.0 - 所有评论" href="<?php bloginfo('comments_rss2_url'); ?>" />
+
+    <!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+	<script src="http://apps.bdimg.com/libs/jquery/2.0.0/jquery.min.js"></script>
+
+	<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+	<script src="http://apps.bdimg.com/libs/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+
+    <!--  添加wp_head  -->
+    <?php wp_head(); ?>
+
+    <!--添加自定义的des和keywords    -->
+    <?php
+    $description = '';
+    $keywords = '';
+
+    if (is_home() || is_page()) {
+        // 将以下引号中的内容改成你的主页description
+        $description = "李经纶个人博客";
+
+        // 将以下引号中的内容改成你的主页keywords
+        $keywords = "李经纶, 博客, 前端 , javascript , angular , node , html5 , css3 , animation ,js";
+    }
+    elseif (is_single()) {
+        $description1 = get_post_meta($post->ID, "description", true);
+        $description2 = str_replace("\n","",mb_strimwidth(strip_tags($post->post_content), 0, 200, "…", 'utf-8'));
+
+        // 填写自定义字段description时显示自定义字段的内容，否则使用文章内容前200字作为描述
+        $description = $description1 ? $description1 : $description2;
+
+        // 填写自定义字段keywords时显示自定义字段的内容，否则使用文章tags作为关键词
+        $keywords = get_post_meta($post->ID, "keywords", true);
+        if($keywords == '') {
+            $tags = wp_get_post_tags($post->ID);
+            foreach ($tags as $tag ) {
+                $keywords = $keywords . $tag->name . ", ";
+            }
+            $keywords = rtrim($keywords, ', ');
+        }
+    }
+    elseif (is_category()) {
+        // 分类的description可以到后台 - 文章 -分类目录，修改分类的描述
+        $description = category_description();
+        $keywords = single_cat_title('', false);
+    }
+    elseif (is_tag()){
+        // 标签的description可以到后台 - 文章 - 标签，修改标签的描述
+        $description = tag_description();
+        $keywords = single_tag_title('', false);
+    }
+    $description = trim(strip_tags($description));
+    $keywords = trim(strip_tags($keywords));
+    ?>
+    <meta name="description" content="<?php echo $description; ?>" />
+    <meta name="keywords" content="<?php echo $keywords; ?>" />
+</head>
+<!--刷新缓存-->
+<?php flush(); ?>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <h1><a href="<?php echo get_option('home'); ?>"><?php bloginfo('name'); ?></a> <small>
+                    你好，欢迎来到我的博客！
+                </small> </h1>
+        </div>
+        <div class="col-md-6">
+            <h6 class="description text-right">
+                <?php bloginfo('description'); ?>
+            </h6>
+        </div>
+    </div>
+</div>
+ <nav class="navbar navbar-default nav-header">
+ <div class="container">
+     <div class="row">
+         <div class="col-md-12">
+
+
+      <ul class="nav navbar-nav">
+         <li class="active"><a href="#">首页</a></li>
+          <li><?php
+              // 获取分类
+              $terms = get_terms('category', 'orderby=name&hide_empty=0' );
+
+              // 获取到的分类数量
+              $count = count($terms);
+              if($count > 0){
+                  // 循环输出所有分类信息
+                  foreach ($terms as $term) {
+                      echo '<li><a href="'.get_term_link($term, $term->slug).'" title="'.$term->name.'">'.$term->name.'</a></li>';
+                  }
+              }
+              ?>
+          </li>
+<!--           -->
+<!--         <li class="dropdown">-->
+<!--            <a href="#" class="dropdown-toggle" data-toggle="dropdown">-->
+<!--               前端技术 <b class="caret"></b>-->
+<!--            </a>-->
+<!--            <ul class="dropdown-menu">-->
+<!--               -->
+<!--<!--               <li><a href="#">HTML</a></li>-->
+<!--<!--               <li><a href="#">CSS</a></li>-->
+<!--<!--               <li><a href="#">Javascript</a></li>-->
+<!--<!--               <li class="divider"></li>-->
+<!--<!--               <li><a href="#">Node</a></li>-->
+<!--<!--               <li class="divider"></li>-->
+<!--<!--               <li><a href="#">Angular</a></li>-->
+<!--            </ul>-->
+<!--         </li>-->
+<!--         <li><a href="#">关于博主</a></li>-->
+      </ul>
+         </div>
+     </div>
+ </div>
+    </nav>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<!-- header.php -->
+<?php get_header(); ?>
+<!-- header.php -->
+	<div class="container">
+		<div class="row">
+			<div class="col-cd-12">
+				<span class="adsf">hehe</span>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
